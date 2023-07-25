ed8e35b19e4994075e3c806bde5a7d7b52c1d630
Artem Suvorov <suvorov.artem@hotmail.com>
2023-07-25 17:26:40 +0500

    add line numbers in code editor

--- TextRpgEditor.Client/src/Panels/CodeEditor.tsx
+++ TextRpgEditor.Client/src/Panels/CodeEditor.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './CodeEditor.css';
 
 const SAMPLE_CODE: string = 
@@ -7,13 +8,39 @@ const SAMPLE_CODE: string =
   '  Message Hello, World!\n' +
   'End Scene\n';
 
-function CodeEditor() {
+function renderLineNumbers(text: string) {
+  var index = 1;
   return (
-    <div className="w-100 h-100"> 
-      <textarea id="code-editor" className="bg-dark w-100 h-100">
-        { SAMPLE_CODE }
-      </textarea>
-    </div>  
+    <ul>
+      {
+        text.split('\n').map(line =>
+          <li className="list-group-item border-0 p-0">{index++}</li>)
+      }
+    </ul>
+  );
+}
+
+function CodeEditor() {
+  const [text, setText] = useState(SAMPLE_CODE);
+
+  return (
+    <div className="w-100 h-100 container">
+      <div className="row h-100">
+        <div className="vertical-line-numbers col-1 d-flex p-2">
+          { renderLineNumbers(text) }
+        </div>
+        <div className="col h-100 d-flex flex-column">
+          <textarea id="code-editor" className="row flex-grow-1 bg-dark h-100 p-2"
+            value={text} onChange={(e) => setText(e.target.value)} />
+          <div className="row py-1 px-2">
+            <ul className="list-inline m-0 d-flex justify-content-between">
+              <li className="list-inline-item">Character count: {text.length}</li>
+              <li className="list-inline-item">Line count: {text.split('\n').length}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
