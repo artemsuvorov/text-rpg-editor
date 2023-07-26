2e338f31f1dbe6734d83f46c1ffd1db81ada4a42
Artem Suvorov <suvorov.artem@hotmail.com>
2023-07-26 18:04:35 +0500

    fix vertical scroll issue with numbered lines

--- TextRpgEditor.Client/src/Panels/CodeEditor.tsx
+++ TextRpgEditor.Client/src/Panels/CodeEditor.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './CodeEditor.css';
 
 const SAMPLE_CODE: string = 
@@ -14,31 +14,44 @@ function renderLineNumbers(text: string) {
     <ul>
       {
         text.split('\n').map(line =>
-          <li className="list-group-item border-0 p-0">{index++}</li>)
+          <li className="list-group-item border-0 p-0" key={index}>{index++}</li>)
       }
     </ul>
   );
 }
 
 function CodeEditor() {
-  const [text, setText] = useState(SAMPLE_CODE);
+  const lineNumbersRef = useRef<HTMLDivElement | null>(null);
+  const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
+  const [text, setText] = useState<string>(SAMPLE_CODE);
+
+  const onTextAreaScroll = (event: React.UIEvent<HTMLTextAreaElement>) => {
+    if (lineNumbersRef.current == null)
+      return;
+
+    const target = event.target as HTMLElement;
+    lineNumbersRef.current.scrollTop = target.scrollTop;
+    console.log(lineNumbersRef.current.scrollTop, target.scrollTop);
+  }
 
   return (
-    <div className="w-100 h-100 container">
-      <div className="row h-100">
-        <div className="vertical-line-numbers col-1 d-flex p-2">
+    <div className="w-100 h-100 container d-flex flex-column">
+      <div id="code-editor-container" className="row h-100">
+        <div className="vertical-line-numbers col-2 p-2" ref={lineNumbersRef}>
           { renderLineNumbers(text) }
         </div>
-        <div className="col h-100 d-flex flex-column">
-          <textarea id="code-editor" className="row flex-grow-1 bg-dark h-100 p-2"
-            value={text} onChange={(e) => setText(e.target.value)} />
-          <div className="row py-1 px-2">
-            <ul className="list-inline m-0 d-flex justify-content-between">
-              <li className="list-inline-item">Character count: {text.length}</li>
-              <li className="list-inline-item">Line count: {text.split('\n').length}</li>
-            </ul>
-          </div>
+        <div className="col d-flex flex-column">
+          <textarea id="code-editor" className="row bg-dark flex-grow-1 p-2"
+            ref={textAreaRef} value={text} 
+            onChange={(event) => setText(event.target.value)} 
+            onScroll={onTextAreaScroll} />
         </div>
+      </div>
+      <div className="row py-1 px-2">
+        <ul className="list-inline m-0 d-flex justify-content-between">
+          <li className="list-inline-item">Character count: {text.length}</li>
+          <li className="list-inline-item">Line count: {text.split('\n').length}</li>
+        </ul>
       </div>
     </div>
   );
